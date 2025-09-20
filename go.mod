be59a56a2a78f21d25c2e174f41eb2ff8278d5ee
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2025-09-20 16:10:07 +0530

    server created

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module webscraper
 
 go 1.23.5
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	
+	godotenv.Load(".env")
+	portString:=os.Getenv("PORT")
+	if portString==""{
+		log.Fatal("PORT Not Found in the Environment variables")
+	}
+	router :=chi.NewRouter()
+	srv:=&http.Server{
+		Handler: router,
+		Addr: ":"+portString,
+
+	}
+	log.Printf("Server Starting on PORT  : %v",portString)
+	err:=srv.ListenAndServe()
+	if err!=nil{
+		log.Fatal("Error : Listning To Server")
+	}
+
 }
